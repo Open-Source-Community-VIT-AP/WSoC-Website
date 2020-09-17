780a4ecba6cd915d5cf5c7eb5e7a4b232015b37c
Akhil Surendran <40662846+akhillllldev@users.noreply.github.com>
2020-09-17 16:05:53 +0530

    Add Balsamiq as Sponsor

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Cards.css";
 import GMCLogo from "../assets/images/GMC_logo.png";
+import BalsamiqLogo from "../assets/images/balsamiq-logo-noborder-screen.png";
 
 function Sponsor() {
   return (
@@ -10,6 +11,11 @@ function Sponsor() {
         <div className="logo_wrapper">
           <a href="https://givemycertificate.com/" target="_blank">
             <img src={GMCLogo}></img>
+          </a>
+        </div>
+    <div className="logo_wrapper">
+          <a href="https://balsamiq.com/" target="_blank">
+            <img src={BalsamiqLogo}></img>
           </a>
         </div>
       </div>
