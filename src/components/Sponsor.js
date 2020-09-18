bbd88322b8a7d5a90b67cc8809ccf0fee98bfd5a
Akhil Surendran <40662846+akhillllldev@users.noreply.github.com>
2020-09-18 23:25:06 +0530

    Add Coding Blocks as Sponsor

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Cards.css";
+import CBLogo from "../assets/images/coding-blocks-logo.png";
 import GMCLogo from "../assets/images/GMC_logo.png";
 import BalsamiqLogo from "../assets/images/balsamiq-logo-noborder-screen.png";
 
@@ -8,6 +9,11 @@ function Sponsor() {
     <div className="cards">
       <h1>Sponsors</h1>
       <div className="cards__container">
+    <div className="logo_wrapper">
+          <a href="https://codingblocks.com/" target="_blank">
+            <img src={CBLogo}></img>
+          </a>
+        </div>
         <div className="logo_wrapper">
           <a href="https://givemycertificate.com/" target="_blank">
             <img src={GMCLogo}></img>
