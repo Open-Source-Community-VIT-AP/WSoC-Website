3382f3c70e37e9ba5e8eac760d3ec036319b43b7
Akhil Surendran <40662846+akhillllldev@users.noreply.github.com>
2020-11-30 19:11:00 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/video3.mp4" autoPlay loop muted />
-      <h1>Winter Is Almost Here!</h1>
-      <p>Click below for Phase 1 Results</p>
+      <h1>Winter Is Here!</h1>
+      <p>Program begins on 4th December</p>
       <div className="hero-btns">
         <Button
           className="btns"
