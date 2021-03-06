4194c7fd73b744f2395499c4dd5abfa47d1cc7b4
akhillllldev <akhil.surendran@vitap.ac.in>
2021-03-06 19:07:33 +0530

    WSoC 2021 update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/pages/Home";
-import Projects from "./components/pages/Projects";
-import Contact from "./components/pages/Contact";
-import Results from "./components/pages/Results";
+import Home from "./components/pages/HomePage/Home";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/results1" component={Results} />
         </Switch>
       </Router>
     </>
---
+++ src/components/pages/HomePage/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="cs">
+            <p className="t1">Winter <br/>Season Of Code</p>
+            <p className="t2">2021</p>
+            <h5>&lt;ComingSoon&gt;01.10.2021&lt;/ComingSoon&gt;</h5>
+        </div>
+    )
+}
+
+export default Home;
