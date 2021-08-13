33cff711d27b78dac0ceca532dd68d55e6bfc01a
Sai Pranay <saipranayadepu07@gmail.com>
2021-08-13 12:23:48 +0530

    updated projects section

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import img from '../../assets/projectimg1.png'
+import img2101 from '../../assets/2101.jpg'
+import img2102 from '../../assets/2102.png'
+import img2103 from '../../assets/2103.svg'
+import img2104 from '../../assets/2104.png'
+import img2105 from '../../assets/2105.png'
 
 import { ProjectContainer, ProjectInner , BigText , ProjectsHeading} from './ProjectElements'
 
@@ -12,44 +17,39 @@ function Projects() {
                   Projects
               </ProjectsHeading>
             <ProjectInner>
-                {/* <ProjectCard 
-                    src={img}
-                    text="Project 1: Productivity Manager"
-                    label="WSOC2001"
+                <ProjectCard 
+                    src={img2101}
+                    text="Project 1 : ClubSpot"
+                    label="WSOC2101"
                     path="#"
                 />
                 <ProjectCard 
-                    src={img}
-                    text="Project 1: Productivity Manager"
-                    label="WSOC2001"
+                    src={img2102}
+                    text="Project 2 : SureNet "
+                    label="WSOC2102"
                     path="#"
                 />
                 <ProjectCard 
-                    src={img}
-                    text="Project 1: Productivity Manager"
-                    label="WSOC2001"
+                    src={img2103}
+                    text="Project 3 : Decentralized Identity Management Platform"
+                    label="WSOC2103"
                     path="#"
                 />
                 <ProjectCard 
-                    src={img}
-                    text="Project 1: Productivity Manager"
-                    label="WSOC2001"
+                    src={img2104}
+                    text="Project 4 : SOUL"
+                    label="WSOC2104"
                     path="#"
                 />
                 <ProjectCard 
-                    src={img}
-                    text="Project 1: Productivity Manager"
-                    label="WSOC2001"
+                    src={img2105}
+                    text="Project 1: Précis"
+                    label="WSOC2105"
                     path="#"
                 />
-                <ProjectCard 
-                    src={img}
-                    text="Project 1: Productivity Manager"
-                    label="WSOC2001"
-                    path="#"
-                /> */}
+                
 
-                <BigText>Coming Soon</BigText>
+                {/* <BigText>Coming Soon</BigText> */}
             </ProjectInner>    
           </ProjectContainer>  
         </>
